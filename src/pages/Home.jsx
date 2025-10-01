2831c67dd38fc82614bdf72fbd59c82cff1badb9
Edu Silva <edusilva18@gmail.com>
2025-10-01 16:23:31 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,40 @@
+import React, { useState } from 'react';
+import { PlantImage, CowImage } from '../assets';
+
+const Home = () => 
+ 
+   [produtos, setProdutos] = useState([ 
+    {
+    
+      id: "7",
+      nome: "Mocha",
+      imagem: "./image/product-07.png",
+      descricao: "Uma descrição completa do produto.",
+      preco: {
+        de: 1000,
+        por: 800,
+      },
+      vegano: false,
+      categoria: "clássicos",
+    },
+    {
+      id: "8",
+      nome: "Latte Vegano",
+      imagem: "./image/product-08.png",
+      descricao: "Feito com leite vegetal.",
+      preco: {
+        de: 1200,
+        por: 1000,
+      },
+      vegano: true,
+      categoria: "especiais",
+    },
+  ]);
+  const categorias = produtos.reduce((acc, produto) => {
+    const categoriaTitulo = produto.categoria;
+  }, {}); 
+
+
 import ProductCard from '../components/ProductCard'
 import '../css/Home.css'
 import produtos from './db.json';
