ebd3b53aea28e911a67100b58ca38f5d6d62c3ee
Pedro Wantuir <pedrowantuirpaixao@gmail.com>
2025-10-17 22:07:06 -0300

    Componente productCard está correto

---
+++ src/components/productCard.jsx
@@ -0,0 +1,34 @@
+import {PlantImage} from '../assets/Plant.png'
+import CowImage from '../assets/Cow.png'
+
+
+const ProductCard = ({
+        id,
+        imagem,
+        nome,
+        price: { por: currentPrice, de: oldPrice },
+        vegano
+    }) => {
+
+
+    const formatPrice = (price) => {
+        return `R$ ${price.toFixed(2).replace('.', ',')}`;
+    }
+
+    const tag = vegano ? {image: PlantImage, text: 'Vegano'} : {image: CowImage, text: 'Contém Lactose'}
+
+    return (
+        <a href={`/product/${id}`}className="products__list--item">
+            <img src={imagem} alt={nome}/>
+            <h3 className="product__list--price">
+                {formatPrice (currentPrice)} {oldPrice && <span>{formatPrice(oldPrice)}</span>}
+            </h3>
+            <h4 className="products__list--name">{nome}</h4>
+            <div className="product__tag">
+                <img src={tag.image}/>
+                <span>{tag.text}</span>
+            </div>           
+        </a>
+    )
+}     
+export default ProductCard
