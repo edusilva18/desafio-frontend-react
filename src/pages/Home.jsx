5a7d3dcc95ed9f473e70f069b839831f4bf26d69
Edu Silva <edusilva18@gmail.com>
2025-10-20 08:52:14 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,47 @@ import { use, useState } from 'react'
 import { produtos } from '../../data'
 import '../css/Home.css'
 import ProductCard from '../components/productCard';
-
-export function Home() {
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Product from './pages/Product';
+ 
   const [produtosExibidos,setProdutosExibidos] = useState(produtos)
 
+  function filtrarCategoria(categoria) {
+    const produtosFiltrados = produtos.filter(produto => produto.categoria === categoria);
+    setProdutosExibidos(produtosFiltrados);
+}
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+export function Home() {
+ function Home() {
+  return <h1>Bem-vindo ao CoffeeEspresso!</h1>;
+}
+
+ 
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ 
 
   return (
     <>    
